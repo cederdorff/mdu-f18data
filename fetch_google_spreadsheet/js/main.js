3de02ba00781efb86de08c38c1e79106d5c9ba9b
Rasmus Cederdorff <rasmus@cederdorff.com>
2019-03-14 13:30:47 +0100

    New project added

---
+++ fetch_google_spreadsheet/js/main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+let sheetId = "14y3hKHPoazIg6slrji_0F_fyg9AmU11wDvOCJqWemOM";
+let sheetNumber = 1;
+let sheetUrl = "https://spreadsheets.google.com/feeds/list/" + sheetId + "/" + sheetNumber + "/public/full?alt=json";
+console.log(sheetUrl);
+
+fetch(sheetUrl)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendPersons(json.feed.entry);
+  });
+
+/*
+Appends json data to the DOM
+*/
+function appendPersons(persons) {
+  console.log(persons);
+  let htmlTemplate = "";
+  for (let person of persons) {
+    htmlTemplate += `
+        <article>
+          <img src="${person['gsx$img']['$t']}">
+          <h4>${person['gsx$name']['$t']}</h4>
+          <p><a href="mailto:${person['gsx$email']['$t']}">${person['gsx$email']['$t']}</a></p>
+        </article>
+      `;
+  }
+  document.querySelector("#persons").innerHTML += htmlTemplate;
+}
